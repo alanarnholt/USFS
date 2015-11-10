0931657808f7fcd74e0187ba664adad656491c01
Sami <Hlebaksm@appstate.edu>
2015-11-10 14:47:23 -0500

    Updated 11/10

--- R-Scripts/Sami/rdata.R
+++ R-Scripts/Sami/rdata.R
@@ -2,7 +2,7 @@ library(xlsx)
 
 #from 1900-2050
 #
-swcalc=read.xlsx("swcalc.xlsx",1,header=F)
+swcalc<-read.xlsx("swcalc.xlsx",1,header=F)
 rownames(swcalc)<-1900:2050
 sw<-function(y,c){
   return(swcalc[y-1899,c])#need to fix
