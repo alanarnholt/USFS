ec22c895f954889cfde6b9906b175e7391ed228a
Ben <jonesb@appstate.edu>
2015-11-25 17:07:20 -0500

    new files

---
+++ var3.R
@@ -0,0 +1,15 @@
+# ##Var #3 P_IM
+# #Annual Imports of wood, and paper products + wood fuel, pulp, recovered paper, roundwood/ chips  
+# var3 <- function(y){
+#   return((Calc_AX(y)+Calc_AY(y)
+#             + Calc_AZ(y)+Calc_BA(y) + Calc_BB(y))*1000)
+# }
+# Calc_AX <- function(y){
+#   return(PRO17*usa_O(y))
+# }
+# usa_O <- function(y){
+#   if (y < 1950){
+#     return(('Hair_1963_Table 2_adj'!H55*'Ince_Table 4'!$S$5+'Hair_1963_Table 2_adj'!J55*'Ince_Table 4'!$T$5)*1000)
+#   }
+# }
+
