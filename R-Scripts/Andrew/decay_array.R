cd2be4df956f7081d1471588f1aefebac2433ac1
Andrew Sullivan <sullivana@appstate.edu>
2016-07-06 12:27:10 -0400

    script update

--- R-Scripts/Andrew/decay_array.R
+++ R-Scripts/Andrew/decay_array.R
@@ -7,51 +7,120 @@ halflives <- halflives[-c(4, 9, 13)]
 
 ##creating blank four-dimensional array to fill
 ##look-up array:
-##1. Decay Type (1 = exponential, 2 = k=2)
+##1. Decay Type (1 = exponential, 2 = k=2, 3 = chi-squared)
 ##2. End Use (refer to spreadsheet - will specify later)
 ##3. Year Put in Use (1 = 1900)
 ##4. Year (1 = 1900)
-##Example: decay_array[1, 3, 50, 100] will produce percentage carbon left after exponential decay of a mobile home built in 1950 at year 2000
-##Example: decay_array[1, 3, 50, ] will produce a vector of percentage carbon left after exponential decay of a mobile home built in 1950 for years 1950 - 2050
-##Example: decay_array[1, 3, , 50] will produce a vector of percentage carbon left after exponential decay of mobile homes built in 1900 - 1950 in the year 1950
+##Example: decay_array[1, 3, 51, 101] will produce percentage carbon left after exponential decay of mobile homes built in 1950 at year 2000
+##Example: decay_array[1, 3, 51, ] will produce a vector of percentage carbon left after exponential decay of mobile homes built in 1950 for years 1950 - end year
+##Example: decay_array[1, 3, , 101] will produce a vector of percentage carbon left after exponential decay of mobile homes built from 1900 to 2000 in the year 2000
 decays <- 4 
 enduses <- 13 
-year <- 151 
-decay_array <- array(0,dim=c(decays, enduses, year, year))
+years <- 151 
+decay_array <- array(0,dim=c(decays, enduses, years, years))
 
 ##filling with decay percentages
 ##exponential (k=1)
-for (i in 1:year)
+for (i in 1:years)
 {
+  g <- function(x) {((x^(k-1))*(exp(-x/h)))/(gamma(k)*(h^k))}
+  k <- 1
   for (j in 1:enduses)
   {
-    for (l in 1:i)
-    {  
-      n <- i
-      k <- 1
-      h <- halflives[l,j] / log(2)
-      g <- function(x) {((x^(k-1))*(exp(-x/h)))/(gamma(k)*(h^k))}
-      decay <- integrate(g, lower=l-1, upper=n)$value
-      decay_array[1, j, l, i] <- 1-decay
+    h <- halflives[i,j] / log(2)
+    for (l in 1:(years - i + 1))
+    {
+      decay <- integrate(g, lower=0, upper=l)$value
+      decay_array[1, j, i, (i + l - 1)] <- 1 - decay
     }
   }
 }
 
 ##k=2
-for (i in 1:year)
+for (i in 1:years)
 {
+  g <- function(x) {((x^(k-1))*(exp(1)^(-x/h)))/(gamma(k)*(h^k))}
+  k <- 2
   for (j in 1:enduses)
   {
-    for (l in 1:i)
+    h <- 1
+    decayval <- 1
+    while(abs(decayval - 0.5) > 1e-14) #finding h for each half life
+    {
+      m <- decayval / 0.5
+      h <- h * m
+      decayval <- integrate(g, lower=0, upper=halflives[i,j])$value
+    }
+    for (l in 1:(years - i + 1))
     {  
-      n <- i
-      k <- 2
-      h <- 
-      g <- function(x) {((x^(k-1))*(exp(1)^(-x/h)))/(gamma(k)*(h^k))}
-      decay <- integrate(g, lower=l-1, upper=n)$value
-      decay_array[2, j, l, i] <- 1-decay
+      decay <- integrate(g, lower=0, upper=l)$value
+      decay_array[2, j, i, (i + l - 1)] <- 1 - decay
     }
   }
 }
 
-#testarray <- array(0,dim=c(5,5,5,5))
+##chi-squared
+for (i in 1:years)
+{
+  g <- function(x) {((x^(k-1))*(exp(1)^(-x/h)))/(gamma(k)*(h^k))}
+  h <- 2
+  for (j in 1:enduses)
+  {
+    k <- 1.2
+    decayval <- 1
+    while(abs(decayval - 0.5) > 1e-1) #finding k for each half life
+                                      #needs greater precision?
+    {
+      m <- decayval / 0.5
+      k <- k * m
+      decayval <- integrate(g, lower=0, upper=halflives[i,j])$value
+    }
+    for (l in 1:(years - i + 1))
+    {  
+      decay <- integrate(g, lower=0, upper=l)$value
+      decay_array[3, j, i, (i + l - 1)] <- 1 - decay
+    }
+  }
+}
+
+##example and plots
+##creating test vector of product
+decay_test <- rep(100, times = enduses)
+
+##multiplying by exponential decay values for year 2016
+exponential_test2016 <- decay_test * decay_array[1, c(1:enduses), 117, c(117:years)]
+
+##multiplying by k=2 decay values for year 2016
+k2_test2016 <- decay_test * decay_array[2, c(1:enduses), 117, c(117:years)]
+
+##plots for exponential vs k=2 amount stored in mobile homes built in 2016 - end year
+plot(x = c(117:years), y = exponential_test2016[3,],
+     type = "l",
+     col = "red",
+     lwd = 3,
+     main = "Decay of Mobile Homes Built in 2016",
+     xlab = "Years (Since 1900)",
+     ylab = "Amount of Carbon Stored")
+lines(x = c(117:years), y = k2_test2016[3,],
+      col = "blue",
+      lwd = 3)
+legend("bottomleft", c("exp", "k=2"),
+       col = c("red", "blue"),
+       lwd = 3)
+
+
+
+
+##finding k in chi-squared work
+#h <- 2
+#k <- 1.2
+#decayval <- 1
+#while(abs(decayval - 0.5) > 0.1) #finding k for each half life
+#{
+#  m <- decayval / 0.5
+#  k <- k * m
+#  decayval <- integrate(g, lower=0, upper=halflives[i,j])$value
+#}
+#print(k)
+
+
