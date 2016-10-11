96669a47a78936d54b45e842e1a6a49c6a19de3a
Hannah Laws <lawshx@appstate.edu>
2016-10-11 13:54:28 -0400

    hannahlaws

--- R-Scripts/Equations/Hannah/HalfLivesAnalysis.R
+++ R-Scripts/Equations/Hannah/HalfLivesAnalysis.R
@@ -3,27 +3,37 @@ library(WOODCARB3R)
 
 halflivesmatters <- halfLives
 # dimnames(halflivesmatters)
-halflivesmatters[,1]<- (1+.9)*halflivesmatters[,1]
-halflivesmatters[,10]<- (1+.9)*halflivesmatters[,10]
-
-finalCarbonContribution(Years = 1990) - finalCarbonContribution(Years = 1990,halflives = halflivesmatters)
 
 halflivesmatters[sample(1:151, 1, replace = TRUE), sample(1:16, 1, replace = TRUE)]
 
-stepping_stones<-c(-.90,-.80,-.70,-.60,-.50,-.40,-.30,-.20,-.10, 0,.10,.20,.30,.40,.50,.60,.70,.80,.90)
+stepping_stones<- seq(-.2,.2,.01);stepping_stones
 
+#Marriott Wardman Park in DC (in maryland, not DC) stuff about carbon March27-30?
 
+FinalCountDown<-finalCarbonContribution()
 
 ptm <- proc.time()
 
 change<-list()
+stonesize<-list()
+
+plot(FinalCountDown)
 
 for (i in 1:100){
-  sample_pebble<-sample(c(1:3,5:8,10:12,14:16),1,replace = TRUE)
+  sample_pebble<-sample(1:13,1,replace = TRUE)
+  stones<-sample(stepping_stones,1,replace = TRUE)
   halflivesmatters <- halfLives
-  halflivesmatters[,sample_pebble] <- (1+ sample(stepping_stones, 1, replace = TRUE))*halflivesmatters[,sample_pebble]
-  change[i] <- finalCarbonContribution(Years = sample_pebble + 1989) - finalCarbonContribution(Years = sample_pebble + 1989, halflives = halflivesmatters)
-  
+  halflivesmatters[,sample_pebble] <- (1+ stones)*halflivesmatters[,sample_pebble]
+  lines(finalCarbonContribution(halflives = halflivesmatters))
+  change[[i]] <- ((finalCarbonContribution(halflives = halflivesmatters) - FinalCountDown)/FinalCountDown)*100
+  stonesize[[i]]<-stones
 }
+lines(FinalCountDown, col = "red")
 proc.time() - ptm
 change
+t(stonesize)
+
+#put in loop
+# (new - control)/control * 100 <- percent change
+
+
