5cf8b711c76783d2b5747e3ac73606a556a493ae
Hannah Laws <lawshx@appstate.edu>
2017-02-10 15:11:11 -0500

    HannahVariance

--- R-Scripts/Hannah/VarianceAnalysis.R
+++ R-Scripts/Hannah/VarianceAnalysis.R
@@ -7,15 +7,27 @@ library(WOODCARB3R)
 #each column in the data frame stores each simulation of applying the random error with the exception of the very first column which is removed after the simulation is complete
 #the variance is then taken from 
 
+
+# fixes to make
+##multiple by randomError only since its centered around 1
+##lower number of simulations to 1000 from 2000 because laptop cannot handle it
+
+#Some halflife classes have larger variance range than others, large difference(1,4) and the smallest difference is with 7(note that this is a much smaller range than the others ie 4,000 instead of 3,000,000), using randomError+1
+#each loop takes approximately 30 minutes
+#running all 13 loops takes roughly 6hours
+
+
+#fixes made, results are still teh same as above, it took roughly 4 hours to complete all simulations for the 13 classes
+
 set.seed(1)
 Histdf<-data.frame(nrow=21)
 
 ptmAll<-proc.time()
 ptm<-proc.time()
-for(i in 1:2000){
+for(i in 1:1000){
   halflivesmatters <- halfLives
   randomError<-rnorm(length(halflivesmatters[,1]),1,.2)
-  halflivesmatters[,1]<- halflivesmatters[,1]*(randomError + 1)
+  halflivesmatters[,1]<- halflivesmatters[,1]*(randomError)
   Histdf<-cbind(Histdf,as.data.frame(finalCarbonContribution(Years=c(1990, 2000, 2010),halflives = halflivesmatters)))
 }
 Histdf<-Histdf[,-1]
@@ -35,7 +47,7 @@ Vardf<-Vardf[,-1]
 hist(as.numeric(Vardf))
 
 
-proc.time() - ptm
+OneTrial<-proc.time() - ptm
 
 
 
@@ -48,10 +60,10 @@ set.seed(1)
 Histdf2<-data.frame(nrow=21)
 
 ptm<-proc.time()
-for(i in 1:2000){
+for(i in 1:1000){
   halflivesmatters <- halfLives
   randomError<-rnorm(length(halflivesmatters[,1]),1,.2)
-  halflivesmatters[,2]<- halflivesmatters[,2]*(randomError + 1)
+  halflivesmatters[,2]<- halflivesmatters[,2]*(randomError)
   Histdf2<-cbind(Histdf2,as.data.frame(finalCarbonContribution(Years=c(1990, 2000, 2010),halflives = halflivesmatters)))
 }
 Histdf2<-Histdf2[,-1]
@@ -78,10 +90,10 @@ set.seed(1)
 Histdf3<-data.frame(nrow=21)
 
 ptm<-proc.time()
-for(i in 1:2000){
+for(i in 1:1000){
   halflivesmatters <- halfLives
   randomError<-rnorm(length(halflivesmatters[,1]),1,.2)
-  halflivesmatters[,3]<- halflivesmatters[,3]*(randomError + 1)
+  halflivesmatters[,3]<- halflivesmatters[,3]*(randomError)
   Histdf3<-cbind(Histdf3,as.data.frame(finalCarbonContribution(Years=c(1990, 2000, 2010),halflives = halflivesmatters)))
 }
 Histdf3<-Histdf3[,-1]
@@ -108,10 +120,10 @@ set.seed(1)
 Histdf4<-data.frame(nrow=21)
 
 ptm<-proc.time()
-for(i in 1:2000){
+for(i in 1:1000){
   halflivesmatters <- halfLives
   randomError<-rnorm(length(halflivesmatters[,1]),1,.2)
-  halflivesmatters[,4]<- halflivesmatters[,4]*(randomError + 1)
+  halflivesmatters[,4]<- halflivesmatters[,4]*(randomError)
   Histdf4<-cbind(Histdf4,as.data.frame(finalCarbonContribution(Years=c(1990, 2000, 2010),halflives = halflivesmatters)))
 }
 Histdf4<-Histdf4[,-1]
@@ -138,10 +150,10 @@ set.seed(1)
 Histdf5<-data.frame(nrow=21)
 
 ptm<-proc.time()
-for(i in 1:2000){
+for(i in 1:1000){
   halflivesmatters <- halfLives
   randomError<-rnorm(length(halflivesmatters[,1]),1,.2)
-  halflivesmatters[,5]<- halflivesmatters[,5]*(randomError + 1)
+  halflivesmatters[,5]<- halflivesmatters[,5]*(randomError)
   Histdf5<-cbind(Histdf5,as.data.frame(finalCarbonContribution(Years=c(1990, 2000, 2010),halflives = halflivesmatters)))
 }
 Histdf5<-Histdf5[,-1]
@@ -168,10 +180,10 @@ set.seed(1)
 Histdf6<-data.frame(nrow=21)
 
 ptm<-proc.time()
-for(i in 1:2000){
+for(i in 1:1000){
   halflivesmatters <- halfLives
   randomError<-rnorm(length(halflivesmatters[,1]),1,.2)
-  halflivesmatters[,6]<- halflivesmatters[,6]*(randomError + 1)
+  halflivesmatters[,6]<- halflivesmatters[,6]*(randomError)
   Histdf6<-cbind(Histdf6,as.data.frame(finalCarbonContribution(Years=c(1990, 2000, 2010),halflives = halflivesmatters)))
 }
 Histdf6<-Histdf6[,-1]
@@ -198,10 +210,10 @@ set.seed(1)
 Histdf7<-data.frame(nrow=21)
 
 ptm<-proc.time()
-for(i in 1:2000){
+for(i in 1:1000){
   halflivesmatters <- halfLives
   randomError<-rnorm(length(halflivesmatters[,1]),1,.2)
-  halflivesmatters[,7]<- halflivesmatters[,7]*(randomError + 1)
+  halflivesmatters[,7]<- halflivesmatters[,7]*(randomError)
   Histdf7<-cbind(Histdf7,as.data.frame(finalCarbonContribution(Years=c(1990, 2000, 2010),halflives = halflivesmatters)))
 }
 Histdf7<-Histdf7[,-1]
@@ -228,10 +240,10 @@ set.seed(1)
 Histdf8<-data.frame(nrow=21)
 
 ptm<-proc.time()
-for(i in 1:2000){
+for(i in 1:1000){
   halflivesmatters <- halfLives
   randomError<-rnorm(length(halflivesmatters[,1]),1,.2)
-  halflivesmatters[,8]<- halflivesmatters[,8]*(randomError + 1)
+  halflivesmatters[,8]<- halflivesmatters[,8]*(randomError)
   Histdf8<-cbind(Histdf8,as.data.frame(finalCarbonContribution(Years=c(1990, 2000, 2010),halflives = halflivesmatters)))
 }
 Histdf8<-Histdf8[,-1]
@@ -258,10 +270,10 @@ set.seed(1)
 Histdf9<-data.frame(nrow=21)
 
 ptm<-proc.time()
-for(i in 1:2000){
+for(i in 1:1000){
   halflivesmatters <- halfLives
   randomError<-rnorm(length(halflivesmatters[,1]),1,.2)
-  halflivesmatters[,9]<- halflivesmatters[,9]*(randomError + 1)
+  halflivesmatters[,9]<- halflivesmatters[,9]*(randomError)
   Histdf9<-cbind(Histdf9,as.data.frame(finalCarbonContribution(Years=c(1990, 2000, 2010),halflives = halflivesmatters)))
 }
 Histdf9<-Histdf9[,-1]
@@ -288,10 +300,10 @@ set.seed(1)
 Histdf10<-data.frame(nrow=21)
 
 ptm<-proc.time()
-for(i in 1:2000){
+for(i in 1:1000){
   halflivesmatters <- halfLives
   randomError<-rnorm(length(halflivesmatters[,1]),1,.2)
-  halflivesmatters[,10]<- halflivesmatters[,10]*(randomError + 1)
+  halflivesmatters[,10]<- halflivesmatters[,10]*(randomError)
   Histdf10<-cbind(Histdf10,as.data.frame(finalCarbonContribution(Years=c(1990, 2000, 2010),halflives = halflivesmatters)))
 }
 Histdf10<-Histdf10[,-1]
@@ -318,10 +330,10 @@ set.seed(1)
 Histdf11<-data.frame(nrow=21)
 
 ptm<-proc.time()
-for(i in 1:2000){
+for(i in 1:1000){
   halflivesmatters <- halfLives
   randomError<-rnorm(length(halflivesmatters[,1]),1,.2)
-  halflivesmatters[,11]<- halflivesmatters[,11]*(randomError + 1)
+  halflivesmatters[,11]<- halflivesmatters[,11]*(randomError)
   Histdf11<-cbind(Histdf11,as.data.frame(finalCarbonContribution(Years=c(1990, 2000, 2010),halflives = halflivesmatters)))
 }
 Histdf11<-Histdf11[,-1]
@@ -348,10 +360,10 @@ set.seed(1)
 Histdf12<-data.frame(nrow=21)
 
 ptm<-proc.time()
-for(i in 1:2000){
+for(i in 1:1000){
   halflivesmatters <- halfLives
   randomError<-rnorm(length(halflivesmatters[,1]),1,.2)
-  halflivesmatters[,12]<- halflivesmatters[,12]*(randomError + 1)
+  halflivesmatters[,12]<- halflivesmatters[,12]*(randomError)
   Histdf12<-cbind(Histdf12,as.data.frame(finalCarbonContribution(Years=c(1990, 2000, 2010),halflives = halflivesmatters)))
 }
 Histdf12<-Histdf12[,-1]
@@ -378,10 +390,10 @@ set.seed(1)
 Histdf13<-data.frame(nrow=21)
 
 ptm<-proc.time()
-for(i in 1:2000){
+for(i in 1:1000){
   halflivesmatters <- halfLives
   randomError<-rnorm(length(halflivesmatters[,1]),1,.2)
-  halflivesmatters[,13]<- halflivesmatters[,13]*(randomError + 1)
+  halflivesmatters[,13]<- halflivesmatters[,13]*(randomError)
   Histdf13<-cbind(Histdf13,as.data.frame(finalCarbonContribution(Years=c(1990, 2000, 2010),halflives = halflivesmatters)))
 }
 Histdf13<-Histdf13[,-1]
@@ -396,7 +408,23 @@ Vardf13<-Vardf13[,-1]
 
 hist(as.numeric(Vardf13))
 
+#difference in the max and min variances of simulations, this way we can tell how big of a range the variance of each halflife is going.
+as.numeric(max(Vardf))-as.numeric(min(Vardf))
+as.numeric(max(Vardf2))-as.numeric(min(Vardf2))
+as.numeric(max(Vardf3))-as.numeric(min(Vardf3))
+as.numeric(max(Vardf4))-as.numeric(min(Vardf4))
+as.numeric(max(Vardf5))-as.numeric(min(Vardf5))
+as.numeric(max(Vardf6))-as.numeric(min(Vardf6))
+as.numeric(max(Vardf7))-as.numeric(min(Vardf7))
+as.numeric(max(Vardf8))-as.numeric(min(Vardf8))
+as.numeric(max(Vardf9))-as.numeric(min(Vardf9))
+as.numeric(max(Vardf10))-as.numeric(min(Vardf10))
+as.numeric(max(Vardf11))-as.numeric(min(Vardf11))
+as.numeric(max(Vardf12))-as.numeric(min(Vardf12))
+as.numeric(max(Vardf13))-as.numeric(min(Vardf13))
 
+
+#max and min of the variances produced in each simulation
 max(Vardf);min(Vardf)
 max(Vardf2);min(Vardf2)
 max(Vardf3);min(Vardf3)
@@ -412,7 +440,7 @@ max(Vardf12);min(Vardf12)
 max(Vardf13);min(Vardf13)
 
 
-proc.time() - ptmAll
+AllOfThem<-proc.time() - ptmAll
 
 
-
+OneTrial;AllOfThem
